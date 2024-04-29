1493672639b4e749ec1d74346da33bcce0e95d47
Usulidin <mr.usuliddin@mai.ru>
2024-04-29 10:31:06 +0500

    welcome icon change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,25 +19,25 @@
     <div class="row px-xl-5 pb-3">
         <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
             <div class="d-flex align-items-center border mb-4" style="padding: 30px;">
-                <h1 class="fa-solid fa-check text-primary m-0 mr-3"></h1>
+                <h1 class="fa fa-solid fa-check text-primary m-0 mr-3"></h1>
                 <h5 class="font-weight-semi-bold m-0">Sifatli mahsulotlar</h5>
             </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
             <div class="d-flex align-items-center border mb-4" style="padding: 30px;">
-                <h1 class="fa-solid fa-truck-fast text-primary m-0 mr-2"></h1>
+                <h1 class="fa fa-solid fa-truck text-primary m-0 mr-2"></h1>
                 <h5 class="font-weight-semi-bold m-0"> Keng turdagi uskunalar</h5>
             </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
             <div class="d-flex align-items-center border mb-4" style="padding: 30px;">
-                <h1 class="fa-solid fa-award text-primary m-0 mr-3"></h1>
+                <h1 class="fa fa-solid fa-award text-primary m-0 mr-3"></h1>
                 <h5 class="font-weight-semi-bold m-0">Eng yaxshi narxlar</h5>
             </div>
         </div>
         <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
             <div class="d-flex align-items-center border mb-4" style="padding: 30px;">
-                <h1 class="fa-solid fa-phone-volume text-primary m-0 mr-3"></h1>
+                <h1 class="fa fa-solid fa-phone-volume text-primary m-0 mr-3"></h1>
                 <h5 class="font-weight-semi-bold m-0">Doimiy qo'llav quvvatlash</h5>
             </div>
         </div>
